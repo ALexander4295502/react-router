c154189b90d39d6fd7a3b68f0cfc8237bbf98086
Zheng Yuan <yuanzheng@sportdex.io>
2018-08-18 15:48:42 -0700

    Render nested routs with react router

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
 
-const Links = () => (
-  <nav>
-    <Link to={"/home"}>Home</Link>
-    <Link to={"/about"}>About</Link>
-    <Link to={"/contact"}>Contact</Link>
-  </nav>
-);
-
-const Header = ({ match }) => (
-  <div className={"header"}>
+const Home = () => <h1>Home</h1>;
+const Menu = () => (
+  <div>
+    <h1>Menu</h1>
+    <Link to={"/menu/food"}>Food</Link>
+    <Link to={"/menu/drink"}>Drinks</Link>
+    <Link to={"/menu/sides"}>Sides</Link>
     <Route
-      path={"/:page"}
-      render={({ match }) => <h1>{match.params.page} header</h1>}
-    />
-  </div>
-);
-
-const Content = ({ match }) => (
-  <div className={"content"}>
-    <Route
-      path={"/:page"}
-      render={({ match }) => <p>{match.params.page} content</p>}
+      path={"/menu/:section"}
+      render={({match}) => <h2>{match.params.section}</h2>}
     />
   </div>
 );
@@ -32,9 +19,10 @@ const Content = ({ match }) => (
 const App = () => (
   <Router>
     <div>
-      <Links />
-      <Header/>
-      <Content/>
+      <Link to="/">Home</Link>
+      <Link to="/menu">Menu</Link>
+      <Route exact path="/" component={Home} />
+      <Route path="/menu" component={Menu} />
     </div>
   </Router>
 );
